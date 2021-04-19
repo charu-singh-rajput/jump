ad40f79246cb4baf0e610382ae8f6e1ce5283d27
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2021-04-19 19:36:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+   
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    
+
+}
+
+function draw() {
+    background("black");
+    
+    drawSprites();
+}
